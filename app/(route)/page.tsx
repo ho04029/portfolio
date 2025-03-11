b5f8033967aa784e94adbcde7ec7eda6f003b491
seokgyeong Lee <ho04029@naver.com>
2025-03-11 14:40:08 +0900

    feat: skills섹션 추가

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,11 +1,13 @@
 import Start from "../_components/Start";
 import AboutMe from "../_components/AboutMe";
+import Skills from "../_components/Skills";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Start />
       <AboutMe />
+      <Skills />
     </div>
   );
 }
---
+++ app/_components/Skills.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Title from "./Title";
+
+const Skills = () => {
+  return (
+    <section>
+      <Title>Skills</Title>
+      <section>
+        <h3>FrontEnd</h3>
+      </section>
+      <section>
+        <h3>BackEnd</h3>
+      </section>
+      <section>
+        <h3>Etc</h3>
+      </section>
+    </section>
+  );
+};
+
+export default Skills;
