b4b7599dfb2e4807561da7efb90930ab077ef6b6
seokgyeong Lee <ho04029@naver.com>
2025-03-17 17:49:18 +0900

    feat: 시작점에 이미지 추가

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -6,7 +6,7 @@ import Project from "../_components/Project";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-items-center">
       <Start />
       <AboutMe />
       <Skills />
--- app/_components/Start.tsx
+++ app/_components/Start.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Start = () => {
   return (
     <section>
-      <h2>Frontend</h2>
+      <img src={"/sky.jpg"} alt="skyBg" className="w-full h-full" />
+      <h2>FRONTEND</h2>
       <h2>PORTFOLIO</h2>
     </section>
   );
