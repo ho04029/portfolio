2bcacf31f650af8e51394acddbb225325caaf951
seokgyeong Lee <ho04029@naver.com>
2025-03-25 18:01:34 +0900

    feat: skills

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import Common from "./Common";
 
 const Skills = () => {
@@ -7,12 +8,55 @@ const Skills = () => {
       <Common.title>Skills</Common.title>
       <section>
         <h3>FrontEnd</h3>
+        <div>
+          <ul>
+            <li>
+              <Image src={"/html.png"} alt="html" height={50} width={50} />
+            </li>
+            <li>
+              <Image src={"/css.png"} alt="css" height={50} width={50} />
+            </li>
+            <li>
+              <Image
+                src={"/javaScript.png"}
+                alt="javascript"
+                height={50}
+                width={50}
+              />
+            </li>
+            <li>
+              <Image src={"/react.png"} alt="react" height={50} width={50} />
+            </li>
+            <li>
+              <Image src={"/nextjs.png"} alt="nextjs" height={50} width={50} />
+            </li>
+            <li>
+              <Image src={"/redux.png"} alt="redux" height={50} width={50} />
+            </li>
+            <li>
+              <Image
+                src={"/zustand.png"}
+                alt="zustand"
+                height={50}
+                width={50}
+              />
+            </li>
+          </ul>
+        </div>
       </section>
       <section>
         <h3>BackEnd</h3>
+        <div>
+          <ul>
+            <li>
+              <Image src={"/nodejs.png"} alt="nodejs" height={50} width={50} />
+            </li>
+          </ul>
+        </div>
       </section>
       <section>
         <h3>Etc</h3>
+        <div></div>
       </section>
     </Common.section>
   );
